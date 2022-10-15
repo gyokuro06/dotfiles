624d4c064ac717c781b454cfb3274ac7fbb6d947
gyokuro06 <gyokuro06@gmail.com>
2022-10-16 00:03:12 +0900

    change setup shell name

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/rc ~/.vim
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-
